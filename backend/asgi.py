1ae7f0f805f50d8a3f2ad9d290ea4792ecb79a1c
Mohamed-Gouse <gouasgauz@gmail.com>
2024-07-29 11:41:43 +0530

    testing asgi.py 6 attepmt

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,13 +2,13 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-# from chat.route import websocket_urlpatterns
-# from channels.auth import AuthMiddlewareStack
-# from chat.jwt_middleware import JWTAuthMiddleware
+from chat.route import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
+from chat.jwt_middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # "websocket": JWTAuthMiddleware(AuthMiddlewareStack(URLRouter(websocket_urlpatterns)))
+    "websocket": JWTAuthMiddleware(AuthMiddlewareStack(URLRouter(websocket_urlpatterns)))
 })
