9770603cd0511840cd5eeca4ab34e5b72db7aa60
Mohamed-Gouse <gouasgauz@gmail.com>
2024-07-29 11:35:50 +0530

    testing asgi.py 5 attepmt

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,9 +2,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chat.route import websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
-from chat.jwt_middleware import JWTAuthMiddleware
+# from chat.route import websocket_urlpatterns
+# from channels.auth import AuthMiddlewareStack
+# from chat.jwt_middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
